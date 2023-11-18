0f79ee77495dac4311dd54594d01735192eece74
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-11-18 10:11:11 -0500

    install devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :update_allowed_parameters, if: :devise_controller?
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone, :role, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone, :role, :email, :password, :password_confirmation])
+  end
 end
